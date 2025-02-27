6a53453fa9c5edba7b40a9da401ae10f22aa6b8e
Balltwance <danupolg@gmail.com>
2025-02-28 00:16:33 +0700

    update

--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -1,30 +1,66 @@
-import React , { useState, useEffect }from 'react';
-import './navbar.css';
-
+import React, { useState, useEffect } from "react";
+import "./navbar.css";
 
 const Navbar = () => {
-    const [active, setActive] = useState("");
-    useEffect(() => {
-      const handleHashChange = () => {
-        setActive(window.location.hash);
-      };
-  
-      window.addEventListener("hashchange", handleHashChange);
-      handleHashChange(); // เรียกครั้งแรกเพื่อกำหนดค่าเริ่มต้น
-  
-      return () => window.removeEventListener("hashchange", handleHashChange);
-    }, []);
-  
-    return (
-      <header>
+  const [active, setActive] = useState("");
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      setActive(window.location.hash);
+    };
+
+    window.addEventListener("hashchange", handleHashChange);
+    handleHashChange(); // เรียกครั้งแรกเพื่อกำหนดค่าเริ่มต้น
+
+    return () => window.removeEventListener("hashchange", handleHashChange);
+  }, []);
+
+  useEffect(() => {
+    const sections = document.querySelectorAll("section");
+
+    const handleScroll = () => {
+      let currentSection = "";
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (window.scrollY >= sectionTop - sectionHeight / 3) {
+          currentSection = `#${section.getAttribute("id")}`;
+        }
+      });
+
+      setActive(currentSection);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <header>
       <ul className="menu">
-        <li><a href="#Home" className={active === "#Home" ? "active" : ""}>Home</a></li>
-        <li><a href="#Education" className={active === "#Education" ? "active" : ""}>Education</a></li>
-        <li><a href="#Skill" className={active === "#Skill" ? "active" : ""}>Skill</a></li>
-        <li><a href="#Contact" className={active === "#Contact" ? "active" : ""}>Contact</a></li>
+        <li>
+          <a href="#Home" className={active === "#Home" ? "active" : ""}>
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#Education" className={active === "#Education" ? "active" : ""}>
+            Education
+          </a>
+        </li>
+        <li>
+          <a href="#Skill" className={active === "#Skill" ? "active" : ""}>
+            Skill
+          </a>
+        </li>
+        <li>
+          <a href="#Contact" className={active === "#Contact" ? "active" : ""}>
+            Contact
+          </a>
+        </li>
       </ul>
     </header>
-    );
+  );
 };
 
 export default Navbar;
